c215e5e999e8842da8fe1375ba7cbbcb808a4aa6
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-03 12:48:14 -0500

    (feature): Add admin, analytics, auth, surveys, and users components with basic templates and tests

--- frontend/surveys/src/app/app.routes.ts
+++ frontend/surveys/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin/admin.component';
+import { AnalyticsComponent } from './admin/analytics/analytics.component';
+import { AuthComponent } from './admin/auth/auth.component';
+import { SurveysComponent } from './admin/surveys/surveys.component';
+import { UsersComponent } from './admin/users/users.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'analytics',
+        component: AnalyticsComponent
+      },
+      {
+        path: 'surveys',
+        component: SurveysComponent
+      }
+    ]
+  }
+];
