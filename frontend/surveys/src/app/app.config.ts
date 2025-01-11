c9acd5e16cf890aefdd761e2e49ffa19ce90d81c
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-10 21:59:59 -0500

    feat: Add PrimeNG theme support, configure animations, and update global styles

--- frontend/surveys/src/app/app.config.ts
+++ frontend/surveys/src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,5 +14,14 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
   ],
 };
