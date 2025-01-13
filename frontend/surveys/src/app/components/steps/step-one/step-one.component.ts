4a3b56bc13129f3386ef3fa07963686e6ae6f02c
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-12 19:00:34 -0500

    feat: Enhance email validation in step one component and improve form control patterns

--- frontend/surveys/src/app/components/steps/step-one/step-one.component.ts
+++ frontend/surveys/src/app/components/steps/step-one/step-one.component.ts
@@ -4,12 +4,12 @@ import { Respondent } from '../../../model/response';
 import { StepperService } from '../../../services/stepper/stepper.service';
 
 @Component({
-    selector: 'app-step-one',
-    imports: [ReactiveFormsModule],
-    templateUrl: './step-one.component.html',
-    styleUrl: './step-one.component.css'
+  selector: 'app-step-one',
+  imports: [ReactiveFormsModule],
+  templateUrl: './step-one.component.html',
+  styleUrl: './step-one.component.css'
 })
-export class StepOneComponent{
+export class StepOneComponent {
   respondentForm: FormGroup;
   respondentData: Respondent = {
     firstname: '',
@@ -32,38 +32,39 @@ export class StepOneComponent{
 
   constructor(public stepperService: StepperService) {
     this.firstname = new FormControl(this.respondentData.firstname, [
-        Validators.required,
-        Validators.maxLength(50),
-        Validators.pattern('^[a-zA-Z ]*$'),
-      ])
-      this.lastname = new FormControl(this.respondentData.lastname, [
-        Validators.required,
-        Validators.maxLength(50),
-        Validators.pattern('^[a-zA-Z ]*$'),
-      ])
-      this.documentType = new FormControl(this.respondentData.documentType, [
-        Validators.required
-      ])
-      this.documentNumber = new FormControl(this.respondentData.documentNumber, [
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-        Validators.minLength(10),
-        Validators.maxLength(20),
-      ])
-      this.email = new FormControl(this.respondentData.email, [
-        Validators.required,
-        Validators.email,
-      ])
-      this.phone = new FormControl(this.respondentData.phone, [
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-        Validators.minLength(10),
-        Validators.maxLength(20),
-      ])
-      this.student = new FormControl(this.respondentData.student, [
-        Validators.required,
-      ])
-    
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.pattern('^[a-zA-Z ]*$'),
+    ])
+    this.lastname = new FormControl(this.respondentData.lastname, [
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.pattern('^[a-zA-Z ]*$'),
+    ])
+    this.documentType = new FormControl(this.respondentData.documentType, [
+      Validators.required
+    ])
+    this.documentNumber = new FormControl(this.respondentData.documentNumber, [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.minLength(10),
+      Validators.maxLength(20),
+    ])
+    this.email = new FormControl(this.respondentData.email, [
+      Validators.required,
+      Validators.email,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+    ])
+    this.phone = new FormControl(this.respondentData.phone, [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.minLength(10),
+      Validators.maxLength(20),
+    ])
+    this.student = new FormControl(this.respondentData.student, [
+      Validators.required,
+    ])
+
     this.respondentForm = new FormGroup({
       firstname: this.firstname,
       lastname: this.lastname,
