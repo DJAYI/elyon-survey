cc3f069e3d4382cd3365cdf9a0db281251b8aa43
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-29 05:00:37 -0500

    feat: Enhance sidebar menu functionality with improved navigation and state persistence

--- frontend/surveys/src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ frontend/surveys/src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -11,17 +11,43 @@ import { AuthenticationService } from '../../services/auth/authentication.servic
     styleUrl: './sidebar-menu.component.css'
 })
 export class SidebarMenuComponent {
-    currentTab = 0;
-    toggleMenu = false;
+    currentTab: number;
+    toggleMenu = true;
     router = inject(Router);
 
-    constructor(public http: HttpClient, public authService: AuthenticationService) { }
+    constructor(public http: HttpClient, public authService: AuthenticationService) {
+        this.currentTab = parseInt(localStorage.getItem('currentTab') || '0');
+
+        if (this.currentTab === 0) {
+            this.router.navigate(['/admin']);
+        } else if (this.currentTab === 1) {
+            this.router.navigate(['/admin/analytics']);
+        } else if (this.currentTab === 2) {
+            this.router.navigate(['/admin/surveys']);
+        } else if (this.currentTab === 3) {
+            this.router.navigate(['/admin/users']);
+        }
+
+        this.toggleMenu = localStorage.getItem('toggleMenu') === 'true';
+    }
     handleCurrentTab(tab: number) {
         this.currentTab = tab;
+
+        localStorage.setItem('currentTab', tab.toString());
+        if (tab === 0) {
+            this.router.navigate(['/admin']);
+        } else if (tab === 1) {
+            this.router.navigate(['/admin/analytics']);
+        } else if (tab === 2) {
+            this.router.navigate(['/admin/surveys']);
+        } else if (tab === 3) {
+            this.router.navigate(['/admin/users']);
+        }
     }
 
     isTabActive(tab: number): boolean {
-        return this.currentTab === tab;
+
+        return this.currentTab === tab
     }
 
     logout() {
@@ -30,6 +56,7 @@ export class SidebarMenuComponent {
 
     toggleSidebar() {
         this.toggleMenu = !this.toggleMenu;
+        localStorage.setItem('toggleMenu', this.toggleMenu.toString());
     }
 }
 
