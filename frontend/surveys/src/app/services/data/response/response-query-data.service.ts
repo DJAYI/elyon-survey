f340e9ebee7184ce9a055f24c4728d983f0c2f4a
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-29 04:59:48 -0500

    refactor: Rename response services and update import paths for improved structure

--- frontend/surveys/src/app/services/data/response/response-query-data.service.ts
+++ frontend/surveys/src/app/services/data/response/response-query-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpResponse } from '../../../../model/http-response';
-import { ResponseSurvey } from '../../../../model/survey-response';
+import { HttpResponse } from '../../../model/http-response';
+import { ResponseSurvey } from '../../../model/survey-response';
 
 @Injectable({
   providedIn: 'root'
