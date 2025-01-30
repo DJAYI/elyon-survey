25c10136e3f2a9e079059c8893d06c8854c5c19e
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2025-01-30 00:24:38 -0500

    feat: Refactor survey table component to use API service and add success/error notifications

--- frontend/surveys/src/app/admin/surveys/survey-table/survey-table.component.ts
+++ frontend/surveys/src/app/admin/surveys/survey-table/survey-table.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { DepartmentEntity, Survey } from '../../../model/survey';
-import { SurveyQueryDataService } from '../../../services/data/surveys/query/implementation/survey-query-data.service';
+import { SurveyQueryApiService } from '../../../services/data/surveys/query/survey-query-api.service';
+import { NotifyService } from '../../../services/utils/notification/notify.service';
 
 @Component({
   selector: 'app-survey-table',
@@ -10,12 +11,30 @@ import { SurveyQueryDataService } from '../../../services/data/surveys/query/imp
   templateUrl: './survey-table.component.html',
   styleUrl: './survey-table.component.css'
 })
-export class SurveyTableComponent {
+export class SurveyTableComponent implements OnInit {
   public surveys: Survey[];
   public departmentEntities: DepartmentEntity[];
 
-  constructor(private surveyQuery: SurveyQueryDataService, private surveyQueryData: SurveyQueryDataService) {
+  constructor(private surveyQuery: SurveyQueryApiService, private notifyService: NotifyService) {
     this.departmentEntities = [];
     this.surveys = [];
   }
+  ngOnInit(): void {
+    this.surveyQuery.getSurveys().subscribe({
+      next: response => {
+        if (response.status === 'success') {
+          this.surveys = response.data as Survey[];
+          this.notifyService.showSuccess('Surveys recovered', 'Surveys have been recovered successfully');
+        } else {
+          this.notifyService.showError('Error recovering surveys', response.message);
+        }
+      },
+
+      error: () => {
+        this.notifyService.showError('Err r recovering surveys', 'An error occurred while trying to recover the surveys');
+      }
+    })
+  }
+
+
 }
